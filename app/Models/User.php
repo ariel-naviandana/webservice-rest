722d7dd02efe42678580693266dbc5f5a5628f48
Ariel2711 <arielnaviandanaputra@gmail.com>
2025-06-01 19:00:02 +0700

    fix throttle

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,8 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class User extends Model
+class User extends Authenticatable
 {
     use HasApiTokens;
     protected $fillable = [
